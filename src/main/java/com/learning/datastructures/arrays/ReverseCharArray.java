95aad3f0663064a44212446ea254ee7ee5e388ba
varun01 <varun.chowdary94@gmail.com>
2021-11-21 20:42:50 +0530

    Initial commit

---
+++ src/main/java/com/learning/datastructures/arrays/ReverseCharArray.java
@@ -0,0 +1,23 @@
+package com.learning.datastructures.arrays;
+
+public class ReverseCharArray {
+
+    public static void main(String[] args) {
+        char[] arr = {'H','a','n','n','a','h'};
+        reverseString(arr);
+    }
+
+    public static void reverseString(char[] s) {
+        int arrLen = s.length;
+        int i = 0;
+        int j = arrLen - 1;
+        while (i < arrLen / 2) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+
+    }
+}
